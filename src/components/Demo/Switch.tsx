01da4d8aa666c61b87842532452b3780a4de70c9
Audi <audiwu321@gmail.com>
2024-03-30 15:24:15 +0800

    feat: remove demo

--- src/components/Demo/Switch.tsx
+++
@@ -1,9 +0,0 @@
-import { Label } from "../ui/label";
-import { Switch } from "../ui/switch";
-
-export const SwitchDemo = () => (
-	<div className="flex items-center space-x-2">
-		<Switch id="airplane-mode" aria-label="Switch" />
-		<Label htmlFor="airplane-mode">Airplane Mode</Label>
-	</div>
-);
