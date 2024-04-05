513d9c72fea75e5b0593720522cb9c5011a8c334
Audi <audiwu321@gmail.com>
2024-04-05 15:18:54 +0800

    feat: test workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,5 +15,7 @@ export default defineConfig({
     sitemap(),
   ],
   output: "server",
-  adapter: vercel(),
+  adapter: node({
+    mode: "standalone",
+  }),
 });
