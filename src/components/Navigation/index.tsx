047326d5df5d2feee684dbf4586aa95a86473cc9
Audi <audiwu321@gmail.com>
2024-04-06 14:10:44 +0800

    use biome check instaed of astro check

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -10,78 +10,78 @@ import { Button } from "../ui/button.tsx";
 import { NavigationItem } from "./item.tsx";
 
 export const Navigation = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [timeline, _] = useState(gsap.timeline({ paused: true, delay: 0.3 }));
-  const { isPlay, setIsPlay } = useAnimationState();
-  const menuRef = useRef<HTMLDivElement>(null);
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+	const [timeline, _] = useState(gsap.timeline({ paused: true, delay: 0.3 }));
+	const { isPlay, setIsPlay } = useAnimationState();
+	const menuRef = useRef<HTMLDivElement>(null);
 
-  const onClick = () => {
-    setIsOpen(!isOpen);
+	const onClick = () => {
+		setIsOpen(!isOpen);
 
-    addAnimation(
-      timeline,
-      gsap.fromTo(
-        menuRef.current,
-        {
-          ...NavAnimation.Navbar.expandAnimationStart,
-          onComplete: () => {
-            setIsPlay(true);
-            document.body.style.overflow = "hidden";
-          },
-        },
-        {
-          ...NavAnimation.Navbar.expandAnimationEnd,
-          onReverseComplete: () => {
-            setIsPlay(false);
-            document.body.style.overflow = "scroll";
-          },
-        }
-      ),
-      0
-    );
+		addAnimation(
+			timeline,
+			gsap.fromTo(
+				menuRef.current,
+				{
+					...NavAnimation.Navbar.expandAnimationStart,
+					onComplete: () => {
+						setIsPlay(true);
+						document.body.style.overflow = "hidden";
+					},
+				},
+				{
+					...NavAnimation.Navbar.expandAnimationEnd,
+					onReverseComplete: () => {
+						setIsPlay(false);
+						document.body.style.overflow = "scroll";
+					},
+				},
+			),
+			0,
+		);
 
-    if (!isOpen) {
-      timeline.play();
-    } else {
-      timeline.reverse(0);
-    }
-  };
+		if (!isOpen) {
+			timeline.play();
+		} else {
+			timeline.reverse(0);
+		}
+	};
 
-  return (
-    <div className="w-full relative">
-      <nav className="flex flex-row justify-between p-8">
-        <h1 className="flex justify-center items-center text-xl font-bold sm:text-3xl">
-          AUDI P.M.
-        </h1>
-        <div className="flex flex-row justify-center items-center gap-2">
-          <ThemeToggle />
-          <Button
-            variant="outline"
-            size="icon"
-            onClick={onClick}
-            disabled={isPlay}
-          >
-            {isOpen ? <X /> : <Menu />}
-          </Button>
-        </div>
-      </nav>
-      <div
-        ref={menuRef}
-        className="hidden w-full absolute top-[104px] z-50 bg-primary-foreground"
-      >
-        <ul className="flex flex-col gap-10 justify-center self-center">
-          {navItems.map((item, index) => (
-            <NavigationItem
-              key={item.id}
-              index={index}
-              href={item.href}
-              value={item.value}
-              timeline={timeline}
-              isLastItem={index === navItems.length - 1}
-            />
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
+	return (
+		<div className="w-full relative">
+			<nav className="flex flex-row justify-between p-8">
+				<h1 className="flex justify-center items-center text-xl font-bold sm:text-3xl">
+					AUDI P.M.
+				</h1>
+				<div className="flex flex-row justify-center items-center gap-2">
+					<ThemeToggle />
+					<Button
+						variant="outline"
+						size="icon"
+						onClick={onClick}
+						disabled={isPlay}
+					>
+						{isOpen ? <X /> : <Menu />}
+					</Button>
+				</div>
+			</nav>
+			<div
+				ref={menuRef}
+				className="hidden w-full absolute top-[104px] z-50 bg-primary-foreground"
+			>
+				<ul className="flex flex-col gap-10 justify-center self-center">
+					{navItems.map((item, index) => (
+						<NavigationItem
+							key={item.id}
+							index={index}
+							href={item.href}
+							value={item.value}
+							timeline={timeline}
+							isLastItem={index === navItems.length - 1}
+						/>
+					))}
+				</ul>
+			</div>
+		</div>
+	);
 };
--- src/layouts/home/index.tsx
+++ src/layouts/home/index.tsx
@@ -1,41 +1,41 @@
 import { Model } from "@/components/model";
+import { Scroll, ScrollControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
-import { Scroll, ScrollControls } from "@react-three/drei";
 
 export const Home = () => {
-  return (
-    <div className="w-full h-screen fixed">
-      <Canvas>
-        <ambientLight intensity={0.1} />
-        <directionalLight color="white" position={[0, 0, 5]} />
-        <Suspense fallback={null}>
-          <ScrollControls pages={1}>
-            <Scroll>
-              <Model />
-            </Scroll>
-            <Scroll html style={{ width: "100%", height: "100vh" }}>
-              <section className="h-[calc(100vh_-_104px)] flex flex-col justify-start sm:justify-center p-12">
-                <div className="">
-                  <h1 className="text-m sm:text-2xl font-normal mb-4">
-                    Hi, my name is
-                  </h1>
-                  <h1 className="text-5xl sm:text-6xl font-black mb-3">
-                    AUDI PUTERA MULIA
-                  </h1>
-                  <h1 className="text-lg sm:text-3xl font-bold mb-3">
-                    WELCOME TO MY PERSONAL WEBSITE
-                  </h1>
-                  <h1 className="text-m sm:text-2xl font-medium sm:w-2/3">
-                    I'm a web developer that enjoys creating things that live on
-                    the internet.
-                  </h1>
-                </div>
-              </section>
-            </Scroll>
-          </ScrollControls>
-        </Suspense>
-      </Canvas>
-    </div>
-  );
+	return (
+		<div className="w-full h-screen fixed">
+			<Canvas>
+				<ambientLight intensity={0.1} />
+				<directionalLight color="white" position={[0, 0, 5]} />
+				<Suspense fallback={null}>
+					<ScrollControls pages={1}>
+						<Scroll>
+							<Model />
+						</Scroll>
+						<Scroll html style={{ width: "100%", height: "100vh" }}>
+							<section className="h-[calc(100vh_-_104px)] flex flex-col justify-start sm:justify-center p-12">
+								<div className="">
+									<h1 className="text-m sm:text-2xl font-normal mb-4">
+										Hi, my name is
+									</h1>
+									<h1 className="text-5xl sm:text-6xl font-black mb-3">
+										AUDI PUTERA MULIA
+									</h1>
+									<h1 className="text-lg sm:text-3xl font-bold mb-3">
+										WELCOME TO MY PERSONAL WEBSITE
+									</h1>
+									<h1 className="text-m sm:text-2xl font-medium sm:w-2/3">
+										I'm a web developer that enjoys creating things that live on
+										the internet.
+									</h1>
+								</div>
+							</section>
+						</Scroll>
+					</ScrollControls>
+				</Suspense>
+			</Canvas>
+		</div>
+	);
 };
