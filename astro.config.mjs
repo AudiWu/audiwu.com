061d9653ce19595c8eb376c6860a7c973ff4e900
Audi <audiwu321@gmail.com>
2025-03-29 13:48:44 +0800

    feat: mdx init

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,23 @@ import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
 import sitemap from "@astrojs/sitemap";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://audiwu.com",
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    sitemap(),
-  ],
+  markdown: {
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      theme: 'dracula'
+    },
+    gfm: true,
+  },
+  integrations: [react(), tailwind({
+    applyBaseStyles: false,
+  }), sitemap(), mdx({
+    gfm: false
+  })],
   output: "server",
   adapter: node({
     mode: "standalone",
