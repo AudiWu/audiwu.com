12d00d16a7e11b40efda9317d6154650b56e4c3a
Audi <audiwu321@gmail.com>
2025-04-19 15:00:19 +0800

    fix: footer css position

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
 	const year = new Date().getFullYear();
 
 	return (
-		<footer className="px-8 py-2 md:py-4 w-full flex justify-center absolute bottom-0 text-center">
+		<footer className="px-8 py-2 md:py-4 w-full flex justify-center text-center">
 			All rights reserved © Audi P.M. {year}
 		</footer>
 	);
